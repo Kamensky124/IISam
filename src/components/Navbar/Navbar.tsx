31b441f8d33652b7e8f12f28497cc5b8556a12e0
Kamensky124 <kamensky24@gmail.com>
2023-01-11 21:07:37 +0700

    repair css in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,39 +2,15 @@ import React from "react";
 import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
 
-// activeClassName - необходимо заменить на className = { navData => navData.isActive ? s.active : s.item }
-// let activeStyle = {
-//     textDecoration: "underline",
-// };
-// let activeClassName = "underline";
-// style={({ isActive }) => isActive ? activeStyle : undefined
-
-type navData = {
-
-}
-
-// {navData => navData.isActive ? s.active : s.item}
-
 const NavBar = () => {
 
     return (<nav className={s.nav}>
-        <div className={`${s.item} ${s.active}`}><NavLink to="/Profile" >Profile</NavLink></div>
-        <div className={s.item}><NavLink to="/Dialogs">Dialogs</NavLink></div>
-        <div className={s.item}><NavLink to="/News">News</NavLink></div>
-        <div className={s.item}><NavLink to="/Music">Music</NavLink></div>
-        <div className={s.item}><NavLink to="/Settings">Settings</NavLink></div>
+        <NavLink className={`${s.item} ${s.active}`} to="/Profile" >Profile</NavLink>
+        <NavLink className={s.item} to="/Dialogs">Dialogs</NavLink>
+        <NavLink className={s.item} to="/News">News</NavLink>
+        <NavLink className={s.item} to="/Music">Music</NavLink>
+        <NavLink className={s.item} to="/Settings">Settings</NavLink>
     </nav>);
-
-    //   nav => nav.isActive ? s.active : s.item*/
-    //     { navData => navData.isActive ? s.active : s.item }
-    // <div className={ nav => nav.isActive ? s.active : s.item }>
-    //     <NavLink to="/Profile">Profile</NavLink>
-    // </div>
-    // <div >
-    //     <NavLink to="/Dialogs">Dialogs</NavLink>
-    // </div>
-    // <div className={`${s.item} ${s.active}`}>*/
-    // <div className={s.item}>*/
 
 }
 
