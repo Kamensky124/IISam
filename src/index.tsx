447bbad2c8221037f631d04eab441cbdfb1b09bb
Kamensky124 <kamensky24@gmail.com>
2023-02-04 22:38:08 +0700

    del render

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
-import {renderTree} from "./Render";
+// import {renderTree} from "./Render";
 import store from "./Redux/State";
+import ReactDOM from "react-dom";
+import App from "./App";
 
+//было в файле render
+const renderTree = () => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App store={store}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+//отрисовка по subscribe
 store.subscribe(renderTree)
+//первончальная отприсовка
 renderTree();
 
 // renderTree(state);
